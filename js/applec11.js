bc71cd9eac7c812542359f9fd00dfaa32cfef65b
Nitin <nksinghal41@gmail.com>
2017-07-16 11:29:33 +0530

    Continution of Expression and Interprotation

--- js/applec11.js
+++ js/applec11.js
@@ -8,8 +8,13 @@ MsgController.$inject = ['$scope'];
 
 function MsgController($scope) {
 $scope.name= "Nitin";
+$scope.stateOfBeing = "env";
 $scope.sayMessage = function(){
   return "Nitin likes to eat healthy pizza";
+};
+$scope.saveEnvironment =  function(){
+$scope.stateOfBeing = "envi";
+
 };
 }
 
