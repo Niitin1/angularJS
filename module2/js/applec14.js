fd0c9bd96ed9b40c68d07b93b6a6b00a096bfdfd
Nitin <nksinghal41@gmail.com>
2017-07-23 11:30:43 +0530

    Using custom Filter and pre defined filters together

---
+++ module2/js/applec14.js
@@ -0,0 +1,41 @@
+(function () {
+'use strict';
+angular.module("MsgApp", [])
+.controller("MsgController" , MsgController)
+.filter("less" , customFactoryFilter)
+.filter("other" , otherCustomFunction);
+
+//here we inject lessFilter, where "less" comes from above staatement as the filter name while "Filter" is/will  append by angular js
+MsgController.$inject=['$scope' , 'lessFilter'];
+function MsgController($scope, lessFilter){
+$scope.stateOfBeing = "env";
+$scope.sayMsg = function(){
+  var msg = "Eating less food";
+  return msg;
+};
+$scope.sayLessMsg = function(){
+  var msg = "Eating less food";
+  msg = lessFilter(msg);
+  return msg;
+};
+$scope.feedNitin = function(){
+$scope.stateOfBeing="envi";
+};
+}
+// Factory method
+function customFactoryFilter() {
+  console.log("Called custom Factory Method");
+return function (input) {
+  input = input || "";
+  input = input.replace("less" ,"more");
+  return input;
+};
+}
+function otherCustomFunction(){
+  return function(input , target, replace){
+    input = input || "";
+    input = input.replace(target ,replace);
+    return input;
+  };
+}
+})();
